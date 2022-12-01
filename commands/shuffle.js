c46b71ecd99c52e1299e68d45ee79282be5b2eb0
LunasHere <lunaashere@gmail.com>
2022-11-30 23:37:40 -0600

    Created shuffle command.

---
+++ commands/shuffle.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shuffle')
+        .setDescription('Shuffles the queue'),
+    async execute(interaction) {
+        // Get the queue
+        const queue = interaction.client.player.getQueue(interaction.guildId)
+        if (!queue || !queue.playing){
+            return await interaction.reply({ content: "There are no songs in the queue", ephemeral: true })
+        }
+
+        // Shuffle the queue
+        queue.shuffle();
+
+        // Create the embed
+        const embed = new EmbedBuilder()
+            .setTitle("CCBot Music")
+            .setDescription("🔀 | Queue shuffled.")
+            .setColor(0x00FF00)
+            .setTimestamp();
+        await interaction.reply({ embeds: [embed] });
+    }
+}
