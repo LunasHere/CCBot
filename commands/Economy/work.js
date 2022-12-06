2db4271859e05b6fc85c4b264d55541b343129dc
LunasHere <lunaashere@gmail.com>
2022-12-05 20:08:58 -0600

    Added work command

---
+++ commands/Economy/work.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const ms = require('ms');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('work')
+        .setDescription('Work for some coins'),
+    async execute(interaction) {
+        const cooldown = await interaction.client.cooldownManager.getCooldown('work', interaction.user);
+        if (cooldown > Date.now()) {
+            return interaction.reply({ content: `You are on cooldown for ${ms(cooldown - Date.now(), { long: true })}!`, ephemeral: true });
+        }
+        const amount = Math.floor(Math.random() * 50) + 1;
+        interaction.client.economyManager.addBalance(interaction.user, amount);
+        interaction.client.cooldownManager.addCooldown('work', interaction.user, 86400000);
+        const embed = new EmbedBuilder()
+            .setTitle('Work')
+            .setDescription(`🪙 | You worked and earned ${amount} coins!`)
+            .setColor(0x00FF00)
+            .setTimestamp();
+        interaction.reply({ embeds: [embed] });
+    }
+};
