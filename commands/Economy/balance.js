1d9d1e96b7596668884b4466776cac2848407df7
LunasHere <lunaashere@gmail.com>
2022-12-05 21:06:20 -0600

    Updated balance command

--- commands/Economy/balance.js
+++ commands/Economy/balance.js
@@ -3,16 +3,9 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('balance')
-        .setDescription('Check your balance')
-        .addUserOption((option) =>
-            option
-                .setName('user')
-                .setDescription('The user to check the balance of')
-                .setRequired(false)
-        ),
+        .setDescription('Check your balance'),
     async execute(interaction) {
-        const user = interaction.options.getUser('user') || interaction.user;
-        const balance = await interaction.client.economyManager.getBalance(user);
+        const balance = await interaction.client.economyManager.getBalance(interaction.user);
         const embed = new EmbedBuilder()
             .setTitle(`${user.username}'s Balance`)
             .setDescription(`🪙 | **Balance:** ${balance} coins`);
