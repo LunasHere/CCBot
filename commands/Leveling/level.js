936115bcbad429940e3127cd8ab57af6354df627
LunasHere <lunaashere@gmail.com>
2022-12-08 03:11:40 +0000

    Updated level command to add user option

--- commands/Leveling/level.js
+++ commands/Leveling/level.js
@@ -3,12 +3,18 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('level')
-        .setDescription('Check your level'),
+        .setDescription('Check your level')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to check')),
     async execute(interaction) {
-        const level = await interaction.client.levelingManager.getLevel(interaction.user);
+        const user = interaction.options.getUser('user') || interaction.user;
+        const level = await interaction.client.levelingManager.getLevel(user);
+        const xp = await interaction.client.levelingManager.getXP(user);
         const embed = new EmbedBuilder()
-            .setTitle(`${interaction.user.username}'s Level`)
-            .setDescription(`📊 | **Level:** ${level}`);
+            .setTitle(`${user.username}'s Level`)
+            .setDescription(`📊 | **Level:** ${level}\n\n**XP:** ${xp}/250`)
+            .setColor(0x00FF00);
         interaction.reply({ embeds: [embed] });
     }
 }
