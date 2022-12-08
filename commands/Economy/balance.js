bbae9222610b847153956424ca971785b68ec587
LunasHere <lunaashere@gmail.com>
2022-12-07 22:22:55 -0600

    Finished updating balance command

--- commands/Economy/balance.js
+++ commands/Economy/balance.js
@@ -7,9 +7,9 @@ module.exports = {
         .addUserOption(option => option.setName('user').setDescription('The user to check the balance of')),
     async execute(interaction) {
         const user = interaction.options.getUser('user') || interaction.user;
-        const balance = await interaction.client.economyManager.getBalance(interaction.user);
+        const balance = await interaction.client.economyManager.getBalance(user);
         const embed = new EmbedBuilder()
-            .setTitle(`${interaction.user.username}'s Balance`)
+            .setTitle(`${user.username}'s Balance`)
             .setDescription(`🪙 | **Balance:** ${balance} coins`);
         interaction.reply({ embeds: [embed] });
     },
