c64cd1ea001abd54db074413c743dc668348b584
LunasHere <lunaashere@gmail.com>
2022-12-07 21:20:34 -0600

    Re-added user option to balance command.

--- commands/Economy/balance.js
+++ commands/Economy/balance.js
@@ -3,8 +3,10 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('balance')
-        .setDescription('Check your balance'),
+        .setDescription('Check your balance')
+        .addUserOption(option => option.setName('user').setDescription('The user to check the balance of')),
     async execute(interaction) {
+        const user = interaction.options.getUser('user') || interaction.user;
         const balance = await interaction.client.economyManager.getBalance(interaction.user);
         const embed = new EmbedBuilder()
             .setTitle(`${interaction.user.username}'s Balance`)
