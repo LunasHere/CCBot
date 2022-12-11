7402da717949b4d05e9e3f3caddbc2e79c49ad24
LunasHere <lunaashere@gmail.com>
2022-12-11 03:03:22 -0600

    Created ticketembed command to create the embed

---
+++ commands/Moderation/ticketembed.js
@@ -0,0 +1,38 @@
+const { EmbedBuilder, ActionRowBuilder, SlashCommandBuilder, PermissionFlagsBits, ButtonBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ticketembed')
+        .setDescription('Creates a ticket embed')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setDMPermission(false),
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+            .setTitle('Ticket')
+            .setDescription('Please click the button below to create a ticket!')
+            .setColor(0xFF0000)
+            .setTimestamp();
+        const row = new ActionRowBuilder()
+            .addComponents([
+                new ButtonBuilder()
+                    .setLabel('Bug Report')
+                    .setStyle(1)
+                    .setEmoji('🐛')
+                    .setCustomId('bug'),
+                
+                new ButtonBuilder()
+                    .setLabel('Report a Player')
+                    .setStyle(1)
+                    .setEmoji('👮')
+                    .setCustomId('report'),
+                    
+                new ButtonBuilder()
+                    .setLabel('Other')
+                    .setStyle(1)
+                    .setEmoji('❓')
+                    .setCustomId('other'),
+            ]);
+        interaction.channel.send({ embeds: [embed], components: [row] });
+        interaction.reply({ content: 'Ticket embed sent!', ephemeral: true });
+    }
+}
