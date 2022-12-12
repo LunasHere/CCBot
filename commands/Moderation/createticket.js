a5595e9f34d4c51ae50e13351aeb88854ecd3920
LunasHere <lunaashere@gmail.com>
2022-12-12 15:33:44 -0600

    Created createticket command

---
+++ commands/Moderation/createticket.js
@@ -0,0 +1,38 @@
+const { EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('createticket')
+        .setDescription('Creates a ticket')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setDMPermission(false)
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to add')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('type')
+                .setDescription('The type of ticket')
+                .setRequired(true)
+                .addChoice('Bug Report', 'bug')
+                .addChoice('Report a Player', 'report')
+                .addChoice('Other', 'other')),
+    async execute(interaction) {
+        // Get the user
+        const user = interaction.options.getUser('user');
+        // Get the type
+        const type = interaction.options.getString('type');
+        // Create the ticket
+        const channel = await interaction.client.ticketManager.createTicket(interaction.guild, user, type, `Ticket created by staff member ${interaction.user.tag}`);
+        // Create embed
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: "CottonCraft Administration", iconURL: "https://i.lunashere.com/cf45a.png" })
+            .setDescription(`This ticket has been created by **${interaction.user}** for **${user}**!`)
+            .setColor(0xFF0000)
+            .setTimestamp();
+        // Send a message to the user
+        channel.send({ embeds: [embed] });
+        // Send a message to the staff
+        interaction.reply({ content: `Ticket created for **${user}**!`, ephemeral: true });
+    }
+}
